7accf487213ff6a0daf565ff1141e913aeb763f3
Ishaan vats <Ishaanvats74@gmail.com>
2025-07-07 03:19:27 +0530

    navbar added

---
+++ app/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='sticky'>
+        <div className=' bg-white/50 flex justify-between items-center mx-5 mt-5 h-20 rounded-full px-5 '>
+            <div className='text-3xl font-semibold '>
+                <p>Resume</p>
+            </div>
+            <div>
+                <ul className='flex space-x-9 text-xl'>
+                    <li className='p-2 rounded-xl hover:bg-gray-100/30 transition-all duration-200 ease-in-out cursor-pointer'>Home</li>
+                    <li className='p-2 rounded-xl hover:bg-gray-100/30 transition-all duration-200 ease-in-out cursor-pointer'>Reviewer</li>
+                    <li className='p-2 rounded-xl hover:bg-gray-100/30 transition-all duration-200 ease-in-out cursor-pointer'>Support</li>
+                </ul>
+            </div>
+            <div className='text-white bg-gray-600 hover:bg-gray-700 transition-all duration-200 ease-in-out px-5 py-4 rounded-full text-xl cursor-pointer'>
+                <p>Sign Up</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
